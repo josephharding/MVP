de9437233fb11b2a1462777595bbf4886c0f6438
jmunsch <james.a.munsch@gmail.com>
2015-06-16 19:38:17 -0500

    Added gulp-util to package.json    Changes to be committed:   	new file:   .jshintrc   	new file:   Gulpfile.js   	modified:   package.json   	new file:   webpack.config.js

---
+++ Gulpfile.js
@@ -0,0 +1,108 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    autoprefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    header  = require('gulp-header'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-minify-css'),
+    gutil = require("gulp-util"),
+    webpack = require("webpack"),
+    WebpackDevServer = require("webpack-dev-server"),
+    webpackConfig = require("./webpack.config.js"),
+
+    package = require('./package.json');
+
+
+var banner = [
+  '/*!\n' +
+  ' * <%= package.name %>\n' +
+  ' * <%= package.title %>\n' +
+  ' * <%= package.url %>\n' +
+  ' * @author <%= package.author %>\n' +
+  ' * @version <%= package.version %>\n' +
+  ' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+  ' */',
+  '\n'
+].join('');
+
+gulp.task('css', function () {
+    return gulp.src('client/scss/style.scss')
+    .pipe(sass({errLogToConsole: true}))
+    .pipe(autoprefixer('last 4 version'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(minifyCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('client/assets/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('js',function(){
+  gulp.src('client/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(header(banner, { package : package }))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(uglify())
+    .pipe(header(banner, { package : package }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(browserSync.reload({stream:true, once: true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init(null, {
+        server: {
+            baseDir: "client"
+        }
+    });
+});
+
+// // The development server (the recommended option for development)
+// gulp.task("default", ["webpack-dev-server"]);
+
+// Build and watch cycle (another option for development)
+// Advantage: No server required, can run app from filesystem
+// Disadvantage: Requests are not blocked until bundle is available,
+//               can serve an old app on refresh
+gulp.task("build-dev", ["webpack:build-dev"], function() {
+  gulp.watch(["app/**/*"], ["webpack:build-dev"]);
+});
+
+// Production build
+gulp.task("build", ["webpack:build"]);
+
+gulp.task("webpack:build", function(callback) {
+  // modify some webpack config options
+  var myConfig = Object.create(webpackConfig);
+  myConfig.plugins = myConfig.plugins.concat(
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+
+  // run webpack
+  webpack(myConfig, function(err, stats) {
+    if(err) throw new gutil.PluginError("webpack:build", err);
+    gutil.log("[webpack:build]", stats.toString({
+      colors: true
+    }));
+    callback();
+  });
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('default', ['css', 'js', 'browser-sync'], function () {
+    gulp.watch("client/scss/*/*.scss", ['css']);
+    gulp.watch("client/js/*.js", ['js']);
+    gulp.watch("client/*.html", ['bs-reload']);
+});
